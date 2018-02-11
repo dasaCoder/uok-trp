3b5111fba2adc7a5675140855c25aeda4cafb1db
Dilusha <dilushadasanayaka@gmail.com>
2018-02-12 04:52:52 +0530

    add nav bar manually and added jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import { CreateRequestComponent } from './components/main/home/request-btns/crea
 import { CheckRequestComponent } from './components/main/home/request-btns/check-request/check-request.component';
 import { RequestFormComponent } from './components/main/request/request-form/request-form.component';
 import { CheckReqStatusComponent } from './components/main/request/check-req-status/check-req-status.component';
-import {MDBBootstrapModule} from 'angular-bootstrap-md/index';
 
 
 
@@ -39,7 +38,7 @@ import {MDBBootstrapModule} from 'angular-bootstrap-md/index';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    MDBBootstrapModule.forRoot()
+    /*MDBBootstrapModule.forRoot()*/
   ],
   providers: [],
   bootstrap: [AppComponent]
