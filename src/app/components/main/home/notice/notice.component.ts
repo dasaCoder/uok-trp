ed689607c86277626b6c7dadb224a49a9bfaf40f
Dilusha <dilushadasanayaka@gmail.com>
2018-02-21 15:34:43 +0530

    modify notice board

--- src/app/components/main/home/notice/notice.component.ts
+++ src/app/components/main/home/notice/notice.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notice.component.css']
 })
 export class NoticeComponent implements OnInit {
-
+  selectedDate = new Date();
   constructor() { }
 
   ngOnInit() {
