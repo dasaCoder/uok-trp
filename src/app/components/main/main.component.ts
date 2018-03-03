47b6cffcd41d0c4fa7142ceb66b5f8433f481bd8
Dilusha <dilushadasanayaka@gmail.com>
2018-03-03 23:53:39 +0530

    create view status component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {slide} from '../../animations';
 
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  animations: [
+    slide
+  ]
 })
 export class MainComponent implements OnInit {
 
