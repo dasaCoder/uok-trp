32e647b448f3f17642e478d9192d871a6856764a
dasaCoder <dilushadasanayaka@gmail.com>
2018-05-21 21:23:15 +0530

    create link to vehicle profile

--- src/app/components/main/admin/request-admin-view/set-vehicle/set-vehicle.component.ts
+++ src/app/components/main/admin/request-admin-view/set-vehicle/set-vehicle.component.ts
@@ -18,6 +18,7 @@ export class SetVehicleComponent implements OnInit {
   reqListOnVehicle; // this will hold the list of reqeust which belongs to vehicle when admin select vehicle
   public selectedVehicle: Vehicle;
   public imageUrl = '/assets/images/pattern/pattern.png';
+
   constructor(private adminService: AdminService) { }
 
   ngOnInit() {
