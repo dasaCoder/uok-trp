3af1990ffc6f740b18ef69aa092351c186685921
Dilusha100 <dilushadasanayaka@gmail.com>
2018-12-18 14:03:39 +0530

    request table configured

--- src/app/components/main/admin/dashboard/requests-table/requests-table.component.ts
+++ src/app/components/main/admin/dashboard/requests-table/requests-table.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { elementAt } from 'rxjs/operators';
+import { RequestService } from '../../../../../services/request.service';
+import { MatDialog } from '@angular/material';
+import { ReqeustPreveiwComponent } from '../reqeust-preveiw/reqeust-preveiw.component';
 
 @Component({
   selector: 'app-requests-table',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestsTableComponent implements OnInit {
 
-  constructor() { }
+  @Input() requestDataSource;
+
+  displayedColumns: string[] = ['refNo', 'to', 'from', 'driver', 'vehicle'];
+
+  constructor(private requestService: RequestService, private dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  loadRequest(refNo) {
+    //alert(refNo);
+    this.requestService.getOneRequest(refNo)
+        .subscribe(request => {
+
+          const dialogRef = this.dialog.open(ReqeustPreveiwComponent, {
+              width: '90%',
+              data: request['msg'][0]
+          });
+
+          dialogRef.afterClosed().subscribe(result => {
+            console.log(`Dialog result: ${result}`);
+          });
+
+    });
+
+
   }
 
 }
