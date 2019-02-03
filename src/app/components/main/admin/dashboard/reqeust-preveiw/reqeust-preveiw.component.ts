5c037946ba0d8f8fbf716d9d80fefb0915a8c3c5
BurbaCodes <dilushadasanayaka@gmail.com>
2019-02-03 07:19:33 +0530

    merge

--- src/app/components/main/admin/dashboard/reqeust-preveiw/reqeust-preveiw.component.ts
+++ src/app/components/main/admin/dashboard/reqeust-preveiw/reqeust-preveiw.component.ts
@@ -86,7 +86,7 @@ export class ReqeustPreveiwComponent implements OnInit {
   }
 
   rejectRequest(refNo) {
-    this.requestService.change_status(refNo, 3)
+    this.requestService.change_status(refNo, 4)
       .subscribe( (response) => {
         console.log(response['msg']);
         this.selectedRequest['status'] = 3;
@@ -109,7 +109,7 @@ export class ReqeustPreveiwComponent implements OnInit {
 
 
   markDocumentedRequest(refNo) {
-    this.requestService.change_status(refNo, 4)
+    this.requestService.change_status(refNo, 3)
       .subscribe( response => {
         //alert('daon');
         console.log(response['msg']);
@@ -118,107 +118,6 @@ export class ReqeustPreveiwComponent implements OnInit {
         this.isChangeOccured = 1;
       });
   }
-
-  printPdf(request) {
-    let content = `
-
-
-          <div style="width:210mm; height:297mm">
-            <div class="container">
-
-            <div class="container-fluid">
-            <div class="text-center"> <br><br>
-
-                  <img src="../../assets/images/logo/logo.png" class="nav-logo" alt="" style="height: 150px">
-                  <h3>කැළණිය විශ්ව විද්‍යාලය - කැළණිය</h3>
-
-
-                   <p > ධාවන වාරයක් සදහා රථයක් ලබා ගැනීමට අවසර ලබා ගැනීම</p>
-                   <br>
-                   <br>
-              </div>
-
-                <div class="">
-
-                <ngx-qrcode qrc-element-type="url" [qrc-value]="ngxQrcode2" ></ngx-qrcode>
-
-                 <div class="row">
-
-                    <div class="col-12">අංකය	:&nbsp;	TRD/${request.refNo}</div>
-                    <div class="col-12">පීඨය	:	&nbsp;${request.dep_unit}</div>
-                    <div class="col-12">ඉල්ලුම්කරුගේ නම  :  ${request.name}</div>
-                    <div class="col-12">ඉල්ලුම්කරුගේ තනතුර	:&nbsp;	${request.position}</div>
-
-                    <br><br>
-                    <div class="col-12" style="border-bottom: 1px solid #000000;">චාරිකාව පිළිබද</div>
-
-                    <div class="col-12" style="padding-top: 10px">ආරම්භක ස්ථානය	:&nbsp;	${request.departure.pickPointAddress}</div>
-                    <div class="col-12">ගමනාන්තය		:&nbsp;	${request.departure.dropPointAddress}</div>
-
-                <br><br>
-                      <div class="col-8">පිටත්වන දිනය :&nbsp; ${request.departure.pickupDate}</div>
-                      <div class="col-4">වේලාව &nbsp; &nbsp; &nbsp; : &nbsp;${request.departure.pickupTime}</div>
-
-                      <div class="col-8">ආපසු පැමිණෙන දිනය:&nbsp; ${request.arrival.dropDate}</div>
-
-                      <div class="col-4">වේලාව &nbsp; &nbsp; &nbsp; :&nbsp; ${request.arrival.dropTime}</div>
-
-                     <br><br>
-
-                    <div class="col-6">හේතුව	&nbsp; &nbsp; &nbsp;		:&nbsp;	${request.purpose}</div>
-
-                    <br><br><br>
-
-
-                  </div>
-
-                  </div>
-                <div class="row">
-                        <div class="col-lg-6">
-                          <p>........................................</p>
-                          <p>ඉල්ලුම්කරුගේ අත්සන</p>
-                        </div>
-
-                         <div class="col-lg-6">
-                          <p>........................................</p>
-                          <p>දිනය</p>
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-
-
-
-            </div>
-
-
-
-
-
-          </div>
-    `;
-
-    document.getElementById('request_form_div').innerHTML = content;
-
-    html2canvas(document.getElementById('request_form_div')).then( canvas => {
-
-      document.getElementById('request_form_div').innerHTML = '';
-
-      let imgData = canvas.toDataURL('image/png');
-      // document.body.appendChild(canvas);
-
-      let doc = new jsPDF('p', 'mm' , 'a4');
-      doc.setFont("helvetica");
-      doc.setFontType("bold");
-      doc.setFontSize(10);
-      var width = 470;
-      let height = doc.internal.pageSize.height;
-      doc.addImage(imgData, 'JPEG', 0 , 0, width, height);
-      doc.save('request.pdf');
-    });
-  }
-
 
   printGatePass(request) {
     let content = `
