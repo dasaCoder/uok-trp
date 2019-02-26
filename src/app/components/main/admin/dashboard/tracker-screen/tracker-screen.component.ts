eb6e210e609f6e5350cf13bc11ab5b832a3a91e2
BurbaCodes <dilushadasanayaka@gmail.com>
2019-02-26 16:16:04 +0530

    fix errors

--- src/app/components/main/admin/dashboard/tracker-screen/tracker-screen.component.ts
+++ src/app/components/main/admin/dashboard/tracker-screen/tracker-screen.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MouseEvent } from '@agm/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Observable } from 'rxjs-compat';
 
 @Component({
   selector: 'app-tracker-screen',
