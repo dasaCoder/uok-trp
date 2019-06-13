a50b78f1e3506d5d9a59886c369fe8159289f4da
BurbaCodes <dilushadasanayaka@gmail.com>
2019-06-13 15:49:50 +0530

    add driver view to settings

--- src/app/components/main/admin/drivers/add/add.component.ts
+++ src/app/components/main/admin/drivers/add/add.component.ts
@@ -17,7 +17,7 @@ export class AddComponent implements OnInit {
   }
   addDriver(driver) {
     this.isSubmited = true;
-
+console.log("driver ob",driver);
     // calling addDriver method in admin service
     if (driver.name !== '' || driver.nic_no !== '' || driver.driving_liecence_no !== '' || driver.telephone !== '' || driver.address !== '') {
       //alert('ok');
@@ -27,7 +27,9 @@ export class AddComponent implements OnInit {
 
           if(response['success']){
             alert('driver added succesfully');
-            location.reload();
+            this.isSubmited = false;
+
+            //location.reload();
           }
         });
     }
